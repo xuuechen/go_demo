7de532f446754aa5217729d371e24d64318dacbe
xuechen <921400914@qq.com>
2020-06-03 23:51:18 +0800

    2020 06/02

---
+++ tenth/main.go
@@ -0,0 +1,65 @@
+package main
+
+import "fmt"
+
+//用面向对象的方法来写一个简单的学生管理系统
+//主要功能：增删改查
+
+var studentMan StudentMan
+
+type StudentMan struct {
+	student []*Student
+}
+
+type Student struct {
+	Id     int
+	Name   string
+	Age    int
+	Gender string
+}
+
+//构造方法
+func NewStudent(id int, name string, age int, gender string) *Student {
+	return &Student{
+		Id:     id,
+		Name:   name,
+		Age:    age,
+		Gender: gender,
+	}
+}
+
+//查看学生信息
+func (s *StudentMan) GetStudentInfo() {
+	for _, v := range studentMan.student {
+		fmt.Printf("stu info: %v ", *v)
+	}
+	fmt.Println()
+}
+
+//添加学生
+func (s *StudentMan) addStudent(stu *Student) {
+	s.student = append(s.student, stu)
+}
+
+//删除学生信息
+func (s *StudentMan) deleteStu(index int) {
+	s.student = append(s.student[:index], s.student[index+1:]...)
+}
+
+func main() {
+
+	studentMan = StudentMan{[]*Student{}}
+	//添加学生信息
+	fmt.Println("添加学生信息")
+	studentMan.addStudent(NewStudent(1, "xiaojia", 24, "male"))
+	studentMan.addStudent(NewStudent(2, "xioaoyi", 15, "female"))
+	studentMan.addStudent(NewStudent(3, "xiaobin", 23, "male"))
+	studentMan.addStudent(NewStudent(4, "xiaoding", 24, "female"))
+
+	//查看学生信息
+	studentMan.GetStudentInfo()
+	//删除学生信息
+	studentMan.deleteStu(0)
+	studentMan.GetStudentInfo()
+
+}
